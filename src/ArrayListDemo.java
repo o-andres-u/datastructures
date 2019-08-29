2083ccdbbc51da56b0394466210151668f2bd406
Oscar Andres <oscarandresu@gmail.com>
2019-08-28 19:23:52 -0500

    Adding new ways to create ArrayList

--- src/ArrayListDemo.java
+++ src/ArrayListDemo.java
@@ -1,33 +1,12 @@
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class ArrayListDemo {
 
   public static void main(String[] args) {
-    String name = "Luz Adriana";
-    String name1 = "Diana";
-    String name2 = "Marlon";
-    String name3 = "Roger";
-    String name4 = "Luvier";
-    String name5 = "Johnatan";
-    String name6 = "Juan Pablo";
-    String name7 = "Erick";
-    String name8 = "Oscar";
-    String name9 = "Luz Adriana"; // Duplicated
-    String name10 = "Diana"; // Duplicated
-
-    List<String> names = new ArrayList<>();
-    names.add(name);
-    names.add(name1);
-    names.add(name2);
-    names.add(name3);
-    names.add(name4);
-    names.add(name5);
-    names.add(name6);
-    names.add(name7);
-    names.add(name8);
-    names.add(name9);
-    names.add(name10);
+    List<String> names = Arrays.asList("Luz Adriana", "Diana", "Marlon", "Roger", "Luvier", "Johnatan", "Juan Pablo", "Erick", "Oscar", "Luz Adriana", "Diana");
+    // Second way to create ArrayList -> new ArrayList with add
+    // Third way to create ArrayList -> new ArrayList() {{ add("Name"); }};
 
     System.out.println(names); // Prints in the order that were added
   }
