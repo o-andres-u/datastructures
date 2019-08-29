3014d49a8db5adde2fe0e87bcf8a7d596ce28258
Oscar Andres <oscarandresu@gmail.com>
2019-08-28 20:23:52 -0500

    Adding StackDemo

---
+++ src/StackDemo.java
@@ -0,0 +1,22 @@
+import java.util.Stack;
+
+public class StackDemo {
+
+  public static void main(String[] args) {
+    Stack<String> names = new Stack<>();
+    names.push("Luz Adriana");
+    names.push("Diana");
+    names.push("Marlon");
+    names.push("Roger");
+    names.push("Luvier");
+    names.push("Johnatan");
+    names.push("Juan Pablo");
+    names.push("Erick");
+    names.push("Oscar");
+    names.push("Luz Adriana");
+    names.push("Diana");
+
+    System.out.println(names);
+  }
+
+}
