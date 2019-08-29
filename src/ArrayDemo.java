968a9f3c635a68d08dad6211084f99ce6f74b7ad
Oscar Andres <oscarandresu@gmail.com>
2019-08-28 19:17:15 -0500

    Adding new ways to declare arrays

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -1,7 +1,9 @@
 public class ArrayDemo {
 
   public static void main(String[] args) {
-    String[] names = {"Luz Adriana", "Diana", "Marlon", "Roger", "Juan Pablo", "Johnatan", "Luvier"};
+    String[] names = {"Luz Adriana", "Diana", "Marlon", "Roger", "Luvier", "Johnatan", "Juan Pablo", "Erick", "Oscar", "Luz Adriana", "Diana"};
+    // Second way to declare arrays -> new String[size]
+    // Third way to declare arrays -> new String[] { ... elements ... }
 
     for (String name: names) {
       System.out.println(name);
