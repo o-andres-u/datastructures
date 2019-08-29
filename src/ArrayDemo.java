40dc8abdf343f0feffc53123dc3fe88d256b115a
Oscar Andres <oscarandresu@gmail.com>
2019-08-28 19:04:21 -0500

    Printing array

---
+++ src/ArrayDemo.java
@@ -0,0 +1,12 @@
+public class ArrayDemo {
+
+  public static void main(String[] args) {
+    String[] names = {"Luz Adriana", "Diana", "Marlon", "Roger", "Juan Pablo", "Johnatan", "Luvier"};
+
+    for (String name: names) {
+      System.out.println(name);
+    }
+
+  }
+
+}
