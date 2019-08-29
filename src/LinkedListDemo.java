00ee0d4c226731cb7f57ab6fd850b35d920caa1b
Oscar Andres <oscarandresu@gmail.com>
2019-08-28 20:17:29 -0500

    Refactoring LinkedList

--- src/LinkedListDemo.java
+++ src/LinkedListDemo.java
@@ -1,12 +1,21 @@
-import java.util.Arrays;
-import java.util.List;
+import java.util.LinkedList;
 
 public class LinkedListDemo {
 
   public static void main(String[] args) {
-    List<String> names = Arrays.asList("Luz Adriana", "Diana", "Marlon", "Roger", "Luvier", "Johnatan", "Juan Pablo", "Erick", "Oscar", "Luz Adriana", "Diana");
-    // Second way to create LinkedList -> new LinkedList with add
-    // Third way to create LinkedList -> new LinkedList() {{ add("Name"); }};
+    LinkedList<String> names = new LinkedList<>();
+    names.add("Luz Adriana");
+    names.add("Diana");
+    names.add("Marlon");
+    names.add("Roger");
+    names.add("Luvier");
+    names.add("Johnatan");
+    names.add("Juan Pablo");
+    names.add("Erick");
+    names.add("Oscar");
+    names.add("Luz Adriana");
+    names.add("Diana");
+    // Second way to create LinkedList -> new LinkedList() {{ add("Name"); }};
 
     System.out.println(names);
   }
