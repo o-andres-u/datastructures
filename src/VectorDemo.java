fddf801bd0b35396a01b6cad2ec42cc4a9c3ea25
Oscar Andres <oscarandresu@gmail.com>
2019-08-28 20:29:01 -0500

    Adding Vector Demo

---
+++ src/VectorDemo.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class VectorDemo {
+
+  public static void main(String[] args) {
+    Vector<String> names = new Vector<>();
+    names.add("Luz Adriana");
+    names.add("Diana");
+    names.add("Marlon");
+    names.add("Roger");
+    names.add("Luvier");
+    names.add("Johnatan");
+    names.add("Juan Pablo");
+    names.add("Erick");
+    names.add("Oscar");
+    names.add("Luz Adriana");
+    names.add("Diana");
+
+    System.out.println(names);
+  }
+
+}
